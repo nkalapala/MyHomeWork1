d912f6c82fdb8b792ca00df11e32ef032f8116b6
nkalapala <narmada_mikki@hotmail.com>
2019-03-05 12:41:24 +0000

    MyHomeWork1

---
+++ src/MyHomeWorkClass1.java
@@ -0,0 +1,15 @@
+package love.programming;
+
+public static void main(String[]args){
+  public class MyHomeWorkClass1 {
+      private String Obj1;
+      private String Obj2;
+
+      public void setObj1Name(String obj1Name){
+          this obj1Name=obj1Name;
+      }
+      public void setObj2Name(String obj2Name){
+          this obj2Name=obj2Name;
+      }
+
+}
